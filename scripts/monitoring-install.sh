9bf609315c16bab4fa01c898a584a9c93f74ba91
hassansaadfr <saadaoui.hassan@laposte.net>
2020-01-07 15:29:50 +0100

    remove heapster and metrics

--- scripts/monitoring-install.sh
+++ scripts/monitoring-install.sh
@@ -5,19 +5,14 @@ set -e
 ARCH=$1
 
 kubectl apply -f /tmp/dashboard-rbac.yaml
-kubectl apply -f /tmp/heapster-rbac.yaml
-kubectl apply -f /tmp/metrics-server-rbac.yaml
+
 
 if [ "$ARCH" == "arm" ]; then
     curl -s https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/alternative/kubernetes-dashboard-arm.yaml | \
     sed -e 's/v2.0.0-alpha0/v1.8.3/g' | \
     kubectl apply -f -;
-    kubectl apply -f /tmp/heapster-arm.yaml;
-    kubectl apply -f /tmp/metrics-server-arm.yaml;
 elif [ "$ARCH" == "x86_64" ]; then
     curl -s -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/alternative/kubernetes-dashboard.yaml | \
     sed -e 's/v2.0.0-alpha0/v1.8.3/g' | \
     kubectl apply -f -;
-    kubectl apply -f /tmp/heapster-amd64.yaml;
-    kubectl apply -f /tmp/metrics-server-amd64.yaml;
 fi
